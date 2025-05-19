718687809725734ee99f2be2b0c5be672f941c46
beanybomber <125334533+shayelorgelly@users.noreply.github.com>
2025-05-19 13:27:43 +1200

    Create dark-mode.ps1

---
+++ dark-mode.ps1
@@ -0,0 +1,4 @@
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0
+Stop-Process -Name explorer -Force
+Start-Process explorer
